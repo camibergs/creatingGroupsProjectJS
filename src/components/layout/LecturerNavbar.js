086a10ae728fca01976c9a9d94a65f361caf2ec9
Camilla Bergs <camillabergantinos20@yahoo.com>
2023-09-21 10:58:16 +0100

    Minor update in Lecturer Navbar

--- src/components/layout/LecturerNavbar.js
+++ src/components/layout/LecturerNavbar.js
@@ -1,8 +1,14 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import "./LecturerNavbar.scss";
 
 function LecturerNavbar() {
   // Initialisation ------------------------------
+  const location = useLocation();
+  const loggedInUser = location.state?.loggedInUser ?? `unknown`;
+
+  // Trim down the email to remove the domain
+  const trimmedUser = loggedInUser?.split("@")[0];
+
   // State ---------------------------------------
   // Handlers ------------------------------------
   // View -------------------------------------
@@ -11,6 +17,10 @@ function LecturerNavbar() {
 
       <div className='navItem'>
         <NavLink to="/lecturerview">Assessments</NavLink>
+      </div>
+
+      <div className="navItemWelcome">
+        <p className="welcome">Welcome {trimmedUser}</p>
       </div>
 
       <div className='logOut'>
